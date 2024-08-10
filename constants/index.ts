5e2dd1c956942bb106e41ab325c9627905bca0c0
Gaid Yusuf <gxy877@student.bham.ac.uk>
2024-08-10 10:14:24 +0100

    Add all project files

---
+++ constants/index.ts
@@ -0,0 +1,122 @@
+import { RxHome, RxPerson, RxDashboard, RxClipboard } from "react-icons/rx";
+
+export const SkillData = [
+  {
+    name: "Python",
+    Image: "/python.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Java",
+    Image: "/java.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "JavaScript",
+    Image: "/js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    name: "HTML 5",
+    Image: "/html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "CSS",
+    Image: "/css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Tailwind CSS",
+    Image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "React",
+    Image: "/react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Django",
+    Image: "/django.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "PostgreSQL",
+    Image: "/postges.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Git",
+    Image: "/git.png",
+    width: 80,
+    height: 80,
+  },
+];
+
+export const Socials = [
+  {
+    name: "Linkedin",
+    src: "/linkedin.svg",
+    href: "https://www.linkedin.com/in/gaidyusuf",
+  },
+  {
+    name: "Github",
+    src: "/github.svg",
+    href: "https://github.com/GaidYusuf",
+  },
+];
+export const Projects = [
+  {
+    title: "Netflix Clone Website",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+    src: "/NetflixWebsite.png",
+  },
+  {
+    title: "Space Themed Website",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+    src: "/SpaceWebsite.png",
+  },
+  {
+    title: "Modern Nextjs Portfolio",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+    src: "/WebPortfolio.png",
+  },
+  {
+    title: "Matrix themed Website",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+    src: "/Matrix.png",
+  },
+];
+
+export const NavLinks = [
+  {
+    name: "/",
+    icon: RxHome,
+    link: "/",
+  },
+  {
+    name: "/my-skills",
+    icon: RxPerson,
+    link: "/my-skills",
+  },
+  {
+    name: "/my-projects",
+    icon: RxDashboard,
+    link: "/my-projects",
+  },
+  {
+    name: "/contact-me",
+    icon: RxClipboard,
+    link: "/contact-me",
+  },
+];
