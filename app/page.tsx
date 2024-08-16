2a906d2a4ad5707e78108d5083bf519e4c5c423a
Gaid Yusuf <gxy877@student.bham.ac.uk>
2024-08-16 20:47:17 +0100

    Deployment Trial 2

--- app/page.tsx
+++ app/page.tsx
@@ -12,21 +12,21 @@ export default function Home() {
       >
         <div className="pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]">
           <h1 className="text-[50px] text-white font-semibold">
-          Hi, I'm
+            Hi, I&apos;m
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
               {" "}
               Gaid Yusuf! 👋
             </span>
           </h1>
           <p className="text-gray-200 hidden md:block mb-1">
-    Welcome to my portfolio! I'm a passionate computer science graduate with a knack for creating innovative solutions.
-  </p>
-  <p className="text-gray-200 hidden md:block mb-1">
-    When I'm not coding, you'll find me exploring new tech trends, contributing to open-source projects, or enjoying a good challenge in problem-solving. I'm always eager to take on new projects and collaborate with others who share my enthusiasm for technology.
-  </p>
-  <p className="text-gray-200 hidden md:block mb-1">
-    Feel free to explore my work and connect with me—let's build something amazing together!
-  </p>
+            Welcome to my portfolio! I&apos;m a passionate computer science graduate with a knack for creating innovative solutions.
+          </p>
+          <p className="text-gray-200 hidden md:block mb-1">
+            When I&apos;m not coding, you&apos;ll find me exploring new tech trends, contributing to open-source projects, or enjoying a good challenge in problem-solving. I&apos;m always eager to take on new projects and collaborate with others who share my enthusiasm for technology.
+          </p>
+          <p className="text-gray-200 hidden md:block mb-1">
+            Feel free to explore my work and connect with me—let&apos;s build something amazing together!
+          </p>
           <div className="flex-col md:flex-row hidden md:flex gap-5">
             <Link
               href="/my-skills"
@@ -38,14 +38,14 @@ export default function Home() {
               href="/my-projects"
               className="rounded-[20px] group relative bg-trasparent px-5 border hover:bg-purple-400 border-white py-3 text-lg text-white max-w-[200px]"
             >
-              <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hver:opacity-20" />
+              <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hover:opacity-20" />
               My Projects
             </Link>
             <Link
               href="/contact-me"
               className="rounded-[20px] group relative bg-trasparent px-5 border hover:bg-purple-400 border-white px-5 py-3 text-lg text-white max-w-[200px]"
             >
-              <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hver:opacity-20" />
+              <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hover:opacity-20" />
               Contact Me
             </Link>
           </div>
@@ -62,13 +62,13 @@ export default function Home() {
 
         <Link
           href="/my-projects"
-          className="rounded-[20px] group bg-trasparent border border-white px-5 py-3 text-lg text-white max-w-[200px]"
+          className="rounded-[20px] group bg-transparent border border-white px-5 py-3 text-lg text-white max-w-[200px]"
         >
           My projects
         </Link>
         <Link
           href="/contact-me"
-          className="rounded-[20px] group bg-trasparent border border-white px-5 py-3 text-lg text-white max-w-[200px]"
+          className="rounded-[20px] group bg-transparent border border-white px-5 py-3 text-lg text-white max-w-[200px]"
         >
           Contact me
         </Link>
--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,37 +1,45 @@
 "use client"; // Add this line at the top of your file
 
 import React, { useState } from "react";
-import emailjs from 'emailjs-com';
+import emailjs from "emailjs-com";
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: ''
+    name: "",
+    email: "",
+    message: "",
   });
 
   const [isSubmitted, setIsSubmitted] = useState(false); // Track if form is submitted
 
-  const handleChange = (e) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    emailjs.send(process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID, process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID, {
-      from_name: formData.name,
-      from_email: formData.email,
-      message: formData.message,
-      to_name: 'Gaid'
-    }, process.env.NEXT_PUBLIC_EMAILJS_USER_ID)
+    emailjs
+      .send(
+        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
+        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
+        {
+          from_name: formData.name,
+          from_email: formData.email,
+          message: formData.message,
+          to_name: "Gaid",
+        },
+        process.env.NEXT_PUBLIC_EMAILJS_USER_ID!
+      )
       .then((response) => {
         setIsSubmitted(true); // Set to true on successful submission
-        setFormData({ name: '', email: '', message: '' });
+        setFormData({ name: "", email: "", message: "" });
       })
       .catch((error) => {
         // Handle errors if needed
-        console.error('Error sending message:', error);
+        console.error("Error sending message:", error);
       });
   };
 
@@ -67,12 +75,16 @@ const ContactForm = () => {
           className="w-full px-4 py-3 text-sm text-black placeholder-gray-400 bg-white border-0 rounded shadow h-32 resize-none"
         />
       </div>
-      <button 
+      <button
         type="submit"
         disabled={isSubmitted} // Disable button if form is submitted
-        className={`px-6 py-3 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear ${isSubmitted ? 'bg-green-500 hover:bg-green-400 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-400'}`}
+        className={`px-6 py-3 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear ${
+          isSubmitted
+            ? "bg-green-500 hover:bg-green-400 cursor-not-allowed"
+            : "bg-blue-500 hover:bg-blue-400"
+        }`}
       >
-        {isSubmitted ? 'Sent' : 'Send a message'}
+        {isSubmitted ? "Sent" : "Send a message"}
       </button>
     </form>
   );
